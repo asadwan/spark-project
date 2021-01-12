67110e464299ba487e40eb90c5858f077d2fbd32
lokeaichirou <32396024+lokeaichirou@users.noreply.github.com>
2021-01-12 22:56:22 +0800

    Add files via upload

--- code/a6_try.py
+++ code/a6_try.py
@@ -5,6 +5,7 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import udf
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # start spark with 1 worker thread
 sc = SparkContext("local[1]")
@@ -16,15 +17,15 @@ spark = SparkSession(sc)
 
 taskEventsSchema = StructType(
     [
-        StructField("timestamp", LongType(), True),
+        StructField("timestamp", IntegerType(), True),
         StructField("missing_info", StringType(), True),
-        StructField("job_id", StringType(), True),
-        StructField("task_index", StringType(), True),
-        StructField("machine_id", StringType(), True),
-        StructField("event_type", LongType(), True),
+        StructField("job_id", IntegerType(), True),
+        StructField("task_index", IntegerType(), True),
+        StructField("machine_id", IntegerType(), True),
+        StructField("event_type", IntegerType(), True),
         StructField("user_name", StringType(), True),
         StructField("task_scheduling_class", LongType(), True),
-        StructField("priority", LongType(), True),
+        StructField("priority", IntegerType(), True),
         StructField("cpu_request", FloatType(), True),
         StructField("memeory_request", FloatType(), True),
         StructField("disk_space_request", FloatType(), True),
@@ -34,18 +35,31 @@ taskEventsSchema = StructType(
 
 taskEventsDf = (
     spark.read.schema(taskEventsSchema)
-        .csv("../data/task_events/*.csv.gz")
+        .csv("../data/task_events_1/*.csv.gz")
         .where(F.col("machine_id").isNotNull() & F.col("job_id").isNotNull())
 )
 # Load tasks_events into dataframe and preprocess the data to remove rows that have null values in columns of interest
+D1 = taskEventsDf.toPandas()
 
-machine_job_Df = taskEventsDf.select("machine_id", "job_id").distinct().groupBy("job_id").count().withColumnRenamed("count", "No.different machine_ids for each job_id")
+machine_job_Df = (
+    taskEventsDf.select("machine_id", "job_id").distinct()
+        .groupBy("job_id").count()
+        .withColumnRenamed("count", "No_different_machine_ids_for_each_job_id")
+        .sort("job_id")
+)
 DataFrame_To_pandas_0 = machine_job_Df.toPandas()
-DataFrame_To_pandas_0.plot(x= "job_id", y = "No.different machine_ids for each job_id")
+DataFrame_To_pandas_0.plot.bar(x="job_id", y="No_different_machine_ids_for_each_job_id")
+
+job_on_single_machine_Df = (
+    machine_job_Df.where(F.col("No_different_machine_ids_for_each_job_id")==1)
+)
+
+print('percentage of jobs only run on single machine:', job_on_single_machine_Df.count()/machine_job_Df.count()*100)
 
 frame = plt.gca()
 # hide x-axis
 frame.axes.get_xaxis().set_visible(False)
-plt.show()
-print('')
 
+plt.show()
+
+print('')
